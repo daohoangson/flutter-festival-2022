0bee185ae9dc04acde31922fcb36845a37b4ddb7
Dao Hoang Son <daohoangson@gmail.com>
2022-03-19 16:40:55 +0700

    Adding Google Maps to a Flutter app.
    https://codelabs.developers.google.com/codelabs/google-maps-in-flutter#3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,10 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // This key has been restricted by Bundle ID
+    GMSServices.provideAPIKey("AIzaSyAahRiAB6PjMMIOrfgO6JyCuCqlLnEW0bw")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/src/map_widget.dart
+++ lib/src/map_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' hide LatLng;
+import 'package:google_maps_flutter/google_maps_flutter.dart' as google;
 import 'package:latlong2/latlong.dart';
+
+final _latLngBenThanh = LatLng(10.7721148, 106.6960897);
 
 class MapWidget extends StatefulWidget {
   final LatLng? center;
@@ -16,9 +20,36 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
+  GoogleMapController? _controller;
+
+  @override
+  void didUpdateWidget(covariant MapWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final center = widget.center;
+    if (center != null && center != oldWidget.center) {
+      _controller?.animateCamera(CameraUpdate.newLatLng(center.toGoogle));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO
-    return const Placeholder();
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: (widget.center ?? _latLngBenThanh).toGoogle,
+        zoom: 15.0,
+      ),
+      myLocationButtonEnabled: false,
+      onMapCreated: _onMapCreated,
+      zoomControlsEnabled: false,
+    );
   }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller = controller;
+  }
+}
+
+extension _LatLng on LatLng {
+  google.LatLng get toGoogle => google.LatLng(latitude, longitude);
 }
